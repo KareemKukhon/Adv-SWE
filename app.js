d3605939d66b918464ad662386bc4bd4435be093
kareem kukhon <kareemkukhon1@gamil.com>
2023-05-17 12:38:38 +0300

    Job Listings

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const userRouter = require("./api/users/userRouter");
 app.use(express.json());
 
 app.use("/api/", userRouter);
-app.use("/api/", userRouter);
 
 // app.get("/", (req, res)=>{
 //     res.json({
